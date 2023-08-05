b9f8a9eb09b801efbe9d62433a318cd7cc8a3da7
Pedro Vinícius <pedrovinilira2005@gmail.com>
2023-08-05 17:39:25 -0300

    Exercício 1 - X-men

--- x-men/xmen.py
+++ x-men/xmen.py
@@ -1,12 +1,12 @@
 import os
 
 os.system('cls')
-tipoPoder = False
+tipoPoder = 0
 
 port = float(input('Forneça sua nota em português: '))
 mat = float(input('Forneça sua nota em matemática: '))
 poder = int(input('Forneça o valor que você recebeu para o poder: '))
-tipoPoder = bool(input('Se você for telepata digite 1, se não for, digite 0.'))
+tipoPoder = int(input('Se você for telepata digite 1, se não for, digite 0.'))
 
 if (port < 8 and mat < 7):
     os.system('cls')
@@ -22,9 +22,9 @@ else:
     os.system('cls')
     if(poder == 10):
         print('ÔMEGA')
-    elif(tipoPoder == True):
+    elif(tipoPoder == 1):
         media = (port+mat+2*poder)/4
         print('Sua média foi: {0}'.format(media))
-    elif(tipoPoder == False):
+    elif(tipoPoder == 0):
         media = (port+mat+poder)/3
         print('Sua média foi: {0}'.format(media))
