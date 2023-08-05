fc4eaddc38a87afce107b3f81566578deb5cadf1
Pedro Vinícius <pedrovinilira2005@gmail.com>
2023-08-05 16:41:36 -0300

    Exercício 4 - Abrir a porta

---
+++ porta-heman/abrirporta.py
@@ -0,0 +1,9 @@
+import random, os
+os.system('cls')
+
+esq= corc= braco= mroxa= senhora= musculoso = random.randrange(20, 51)
+
+if(senhora + musculoso + 120 < esq + corc+ braco + mroxa):
+    print('O quarteto conseguiu abrir a porta!')
+else:
+    print('O quarteto não conseguiu abrir a porta...')
