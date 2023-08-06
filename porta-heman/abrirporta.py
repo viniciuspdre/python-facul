aa54f3a7b3e079944ee30fbcbc91b2c5f00ae041
Pedro Vinícius <pedrovinilira2005@gmail.com>
2023-08-05 21:09:50 -0300

    Exercicio 4 - He-Man

--- porta-heman/abrirporta.py
+++ porta-heman/abrirporta.py
@@ -1,7 +1,12 @@
 import random, os
 os.system('cls')
 
-esq= corc= braco= mroxa= senhora= musculoso = random.randrange(20, 51)
+esq = random.randrange(20, 51)
+corc = random.randrange(20, 51)
+braco = random.randrange(20, 51)
+mroxa = random.randrange(20, 51)
+senhora = random.randrange(20, 51)
+musculoso = random.randrange(20, 51)
 
 if(senhora + musculoso + 120 < esq + corc+ braco + mroxa):
     print('O quarteto conseguiu abrir a porta!')
