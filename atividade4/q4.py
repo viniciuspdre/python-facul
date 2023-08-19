6298239e16edeeebcfc44d009b8c8eb325e856c9
Pedro Vinícius <pedrovinilira2005@gmail.com>
2023-08-19 17:25:54 -0300

    Flautista

--- atividade4/q4.py
+++ atividade4/q4.py
@@ -4,10 +4,12 @@ os.system('cls')
 
 melodia1 = random.randint(1,10); melodia2 = random.randint(1,10); melodia3 = random.randint(1,10)
 tentativa1 = random.randint(1,10); tentativa2 = random.randint(1,10); tentativa3 = random.randint(1,10)
+falha = 0
 
 while melodia1 != tentativa1 or melodia2 != tentativa2 or melodia3 != tentativa3:
     tentativa1 = random.randint(1,10); tentativa2 = random.randint(1,10); tentativa3 = random.randint(1,10)
+    falha += 1
     
 
 print('Primeira melodia mágica:',melodia1,'\nSegunda melodia mágica:',melodia2,'\nTerceira melodia mágica:',melodia3)
-print('Primeira tentativa do flautista:',tentativa1,'\nSegunda tentativa do flautista:',tentativa2,'\nTerceira tentativa do flautista:',tentativa3)
+print('Primeira tentativa do flautista:',tentativa1,'\nSegunda tentativa do flautista:',tentativa2,'\nTerceira tentativa do flautista:',tentativa3, '\n\nNúmero de tentativas',falha)
