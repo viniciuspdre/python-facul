2445e774915476df2bcb9da672a07b427b5fc35e
Pedro Vinícius <pedrovinilira2005@gmail.com>
2023-08-19 17:48:55 -0300

    Nome com a idade par mais velho

--- atividade4/q6.py
+++ atividade4/q6.py
@@ -9,9 +9,12 @@ confirm = ''
 while confirm != 'sim':
     thisNome = input('Forneça o nome da pessoa: ')
     thisIdade = int(input('Forneça a idade da pessoa: '))
-    if(thisIdade > idade):
-        idade = thisIdade
-        nome = thisNome
+    if(thisIdade % 2 == 0):
+        if(thisIdade > idade):
+            nome = thisNome
+            idade = thisIdade
+    else:
+        print('Essa pessoa não tem idade par.')
     confirm = input('Você deseja sair do programa? (Se sim, digite sim todo em minúsculo, se não, digite qualquer coisa.)')
 
-print('O nome da pessoa mais velha é: ', nome)
+print('O nome da pessoa mais velha com idade par é: ', nome)
